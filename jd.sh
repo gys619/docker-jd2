bf6a138b5b62fcf201688e2880da7d315cab7cec
kangwenhang <kangwenhang@hotmail.com>
2021-02-13 22:46:06 +0800

    update

--- jd.sh
+++ jd.sh
@@ -4,6 +4,7 @@
 ShellDir=${JD_DIR:-$(cd $(dirname $0); pwd)}
 [ ${JD_DIR} ] && HelpJd=jd || HelpJd=jd.sh
 ScriptsDir=${ShellDir}/scripts
+ScriptsDir2=${ShellDir}/scripts2
 ConfigDir=${ShellDir}/config
 FileConf=${ConfigDir}/config.sh
 FileConfSample=${ShellDir}/sample/config.sh.sample
@@ -185,7 +186,7 @@ function Run_Normal {
   
   FileNameTmp1=$(echo $1 | perl -pe "s|\.js||")
   FileNameTmp2=$(echo $1 | perl -pe "{s|jd_||; s|\.js||; s|^|jd_|}")
-  SeekDir="${ScriptsDir} ${ScriptsDir}/backUp ${ConfigDir}"
+  SeekDir="${ScriptsDir} ${ScriptsDir}/backUp ${ScriptsDir2}/commands ${ConfigDir}"
   FileName=""
   WhichDir=""
 
@@ -211,7 +212,7 @@ function Run_Normal {
     cd ${WhichDir}
     node ${FileName}.js | tee ${LogFile}
   else
-    echo -e "\n在${ScriptsDir}、${ScriptsDir}/backUp、${ConfigDir}三个目录下均未检测到 $1 脚本的存在，请确认...\n"
+    echo -e "\n在${ScriptsDir}、${ScriptsDir}/backUp、${ScriptsDir2}/commands、${ConfigDir}四个目录下均未检测到 $1 脚本的存在，请确认...\n"
     Help
   fi
 }
